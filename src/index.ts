cb2439347c6b43d2f195a7e9b6dff6a164dd1649
Sergey Shandar <sergey.shandar@gmail.com>
2018-12-02 06:57:51 -0800

    MainObjectType Tuple2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as schema from "@ts-common/schema"
 import * as json from "@ts-common/json"
+import * as tuple from "@ts-common/tuple"
 
 export type SimpleTypes<T extends schema.SimpleTypes> =
   T extends "string" ? string :
@@ -8,4 +9,23 @@ export type SimpleTypes<T extends schema.SimpleTypes> =
   T extends "null" ? null :
   T extends "array" ? json.JsonArray :
   T extends "object" ? json.JsonObject :
+  never
+
+export type SimpleTypesUnknown<T> =
+  T extends schema.SimpleTypes ? SimpleTypes<T> :
+  never
+
+export type SchemaMainObjectType =
+  undefined|
+  schema.SimpleTypes|
+  tuple.Tuple0|
+  tuple.Tuple1<schema.SimpleTypes>|
+  tuple.Tuple2<schema.SimpleTypes, schema.SimpleTypes>
+
+export type MainObjectType<T extends SchemaMainObjectType> =
+  T extends undefined ? json.Json : // or `SimpleTypes<schema.SimpleTypes>
+  T extends schema.SimpleTypes ? SimpleTypes<T> :
+  T extends tuple.Tuple0 ? never :
+  T extends tuple.Tuple1<infer T0> ? SimpleTypesUnknown<T0> :
+  T extends tuple.Tuple2<infer T0, infer T1> ? SimpleTypesUnknown<T0>|SimpleTypesUnknown<T1> :
   never
--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@ import * as index from "./index"
 import * as assert from "assert"
 import * as json from "@ts-common/json"
 
-describe("compile time test", () => {
+describe("SchemaObjectType", () => {
   it("number", () => {
     const x: index.SimpleTypes<"number"> = 5
     const r: number = x
@@ -30,12 +30,70 @@ describe("compile time test", () => {
   })
   it("object", () => {
     const x: index.SimpleTypes<"object"> = {}
-    const r: object = x
+    const r: json.JsonObject = x
     assert.deepStrictEqual(r, {})
   })
   it("array", () => {
     const x: index.SimpleTypes<"array"> = []
     const r: json.JsonArray = x
     assert.deepStrictEqual(r, [])
+  })
+})
+
+describe("MainObjectType", () => {
+  it("undefined", () => {
+    const x: index.MainObjectType<undefined> = "something"
+    const r: json.Json = x
+    assert.deepStrictEqual(r, "something")
+  })
+  it("number", () => {
+    const x: index.MainObjectType<"number"> = 5
+    const r: number = x
+    assert.strictEqual(r, 5)
+  })
+  it("integer", () => {
+    const x: index.MainObjectType<"integer"> = 5
+    const r: number = x
+    assert.strictEqual(r, 5)
+  })
+  it("string", () => {
+    const x: index.MainObjectType<"string"> = "s"
+    const r: string = x
+    assert.strictEqual(r, "s")
+  })
+  it("null", () => {
+    const x: index.MainObjectType<"null"> = null
+    const r: null = x
+    assert.strictEqual(r, null)
+  })
+  it("boolean", () => {
+    const x: index.MainObjectType<"boolean"> = false
+    const r: boolean = x
+    assert.strictEqual(r, false)
+  })
+  it("object", () => {
+    const x: index.MainObjectType<"object"> = {}
+    const r: json.JsonObject = x
+    assert.deepStrictEqual(r, {})
+  })
+  it("array", () => {
+    const x: index.MainObjectType<"array"> = []
+    const r: json.JsonArray = x
+    assert.deepStrictEqual(r, [])
+  })
+  it("[]", () => {
+    const x: index.MainObjectType<[]> = undefined as never
+    const r: never = x
+    assert.deepStrictEqual(r, undefined)
+  })
+  it('["string"]', () => {
+    const x: index.MainObjectType<["string"]> = "some string"
+    const r: string = x
+    assert.deepStrictEqual(r, "some string")
+  })
+  it(`["integer", "boolean"]`, () => {
+    const x: index.MainObjectType<["integer", "boolean"]> = 56
+    const r: number|boolean = x
+    assert.deepStrictEqual(r, 56)
   })
 })
